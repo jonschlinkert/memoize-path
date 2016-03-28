d97fdb6c712ff0affe4c714af904a580319efa09
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-27 23:30:35 -0400

    use getter for `path`

--- index.js
+++ index.js
@@ -13,16 +13,29 @@ module.exports = function create(dir) {
   dir = dir || process.cwd();
 
   return (function(cwd) {
+    var current;
+
     function memo(str) {
       if (typeof str === 'string') {
         var dir = path.resolve(cwd, str);
         var fn = create(dir);
+        fn.parent = cwd;
         fn.path = dir;
         return fn;
       }
       return path.resolve(cwd);
     }
-    memo.path = path.resolve(cwd);
+
+    Object.defineProperty(memo, 'path', {
+      configurable: true,
+      enumerable: true,
+      set: function(val) {
+        current = val;
+      },
+      get: function() {
+        return path.resolve(current || cwd);
+      }
+    });
     return memo;
   }(dir));
 };
